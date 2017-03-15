3e4fc09f9923a50e500a4cddec018263d7903cd0
snmnima <snmnima@gmail.com>
2017-03-15 11:06:07 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
 		"fun",
 		"info"
 	},
-    sudo_users = {157059515},
+    sudo_users = {95229319},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
